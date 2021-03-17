db1f585f30e78e1e4186b03c916fec3d3b2c888e
rouan <rouan.laporal@outlook.com>
2021-03-17 21:30:51 +0100

    suppression du traitement sur la page d'affichage

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
     include 'config/teamplate/head.php';
+    include 'traitement_login.php';
 ?>
 <header>
 <?php
@@ -19,40 +20,3 @@
     <h5>Nouveau sur DeckedUP?</h5>
     <a href="inscription.php"><input type="button" class="inscription-btn" value="Inscrivez-vous!"></a>
 </div>
-<?php
-    if (isset($_SESSION['user']))
-    {
-        header('location:profil.php?connect=forbidden');
-        exit();
-    }
-    $content ="";
-    if (isset($_GET['access']) &&  $_GET['access'] == 'forbidden')
-    {
-        $content .= '<div style="background:tomato;padding:2%;">Pour accéder à la page profil, vous devez être connecté </div>';
-    }
-    if (isset($_POST['submit']))
-    {
-        extract($_POST);
-        if($pseudo == 'admin' && $password == 'admin')
-        {
-            session_start();
-            $_SESSION['user']['pseudo']=$pseudo;
-            $_SESSION['user']['mdp'] = $password;
-            $_SESSION['user']['statut']=1;
-            header('location:profil.php');
-            exit();
-        }
-        else if(connectPseudo($pseudo,$pdo)==false || passwordConnect($password,$pseudo,$pdo)==false)
-        {
-            echo '<div class="error" style="color:red;">L\identifiant ou le mot de passe ne correspondent pas</div>';
-        }
-        else
-        {
-            session_start();
-            $_SESSION['user']['pseudo']=$pseudo;
-            $_SESSION['user']['mdp'] = $password;
-            $_SESSION['user']['statut']=0;
-            header('location:profil.php');
-            exit();
-        }
-    }
