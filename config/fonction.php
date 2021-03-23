6f2736e326729470908f12e113d61101e6fa44f1
rouan <rouan.laporal@outlook.com>
2021-03-23 21:28:59 +0100

    modification des fonctions d'affichafe

--- config/fonction.php
+++ config/fonction.php
@@ -1,12 +1,10 @@
 <?php
-    function check_mdp($mdp,$conf)
-    {
+    function check_mdp($mdp,$conf){
         $majuscule = preg_match('@[A-Z]@', $mdp);
         $minuscule = preg_match('@[a-z]@', $mdp);
         $chiffre = preg_match('@[0-9]@', $mdp);
         $special = preg_match('@[$%?!]@', $mdp);
         $comparaison = preg_match('/'.$conf.'/',$mdp);
-//iconv_strlen/page admin gestion stock et prix
        if(!$majuscule || !$minuscule || !$chiffre || !$special || !$comparaison || iconv_strlen($mdp)<10 || iconv_strlen($mdp) > 20)
        {
            return false;
@@ -17,12 +15,10 @@
        }
     }
 
-    function check_pseudo($pseudo)
-    {
-        $pdo = new PDO('mysql:host=localhost;dbname=bd_card;','root','aZERTYUIOP_973',[PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
+    function check_pseudo($pseudo,$pdo){
         $result = $pdo ->query('SELECT * FROM tb_user');
         $user = $result->fetch(PDO::FETCH_ASSOC);
-        if ($user['pseudo'] == $pseudo){
+        if ($user == $pseudo){
             return false;
         }
         else
@@ -31,31 +27,27 @@
         }
     }
 
-    function addUser($pseudo,$email,$mdp,$genre,$adresse,$num)
-    {
-        $pdo = new PDO('mysql:host=localhost;dbname=bd_card;','root','aZERTYUIOP_973',[PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
-        $stmt = $pdo -> prepare('INSERT INTO tb_user(pseudo, mdp, email,gender,adresse,tel) VALUES(?,?,?,?,?,?)');
+    function addUser($pdo,$pseudo,$email,$mdp,$genre,$adresse,$num,$statut){
+        $stmt = $pdo -> prepare('INSERT INTO tb_user(pseudo, mdp, email,gender,adresse,tel,statut) VALUES(?,?,?,?,?,?,?)');
         $stmt -> bindParam(1, $pseudo);
         $stmt -> bindParam(2, $mdp);
         $stmt -> bindParam(3, $email);
         $stmt -> bindParam(4, $genre);
         $stmt -> bindParam(5, $adresse);
         $stmt -> bindParam(6, $num);
+        $stmt -> bindParam(7,$statut);
         $stmt -> execute();
     }
 
-    function connectPseudo($pseudo)
-    {
-        $pdo = new PDO('mysql:host=localhost;dbname=bd_card;','root','aZERTYUIOP_973',[PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
+    function connectPseudo($pseudo,$pdo){
         $stmt = $pdo -> prepare('SELECT * FROM tb_user WHERE pseudo = ?');
         $stmt -> bindParam(1,$pseudo);
         $stmt-> execute();
         return $stmt;
     }
 
-    function passwordConnect($password, $pseudo)
+    function passwordConnect($password, $pseudo, $pdo)
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=bd_card;','root','aZERTYUIOP_973',[PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
         $stmt = $pdo -> prepare(('SELECT mdp FROM tb_user WHERE pseudo = ?'));
         $stmt -> bindParam(1, $pseudo);
         $stmt -> execute();
@@ -63,5 +55,37 @@
         $hash = $stmt[0];
         return password_verify($password,$hash);
        
+    }
+    function affichageTel($pdo,$pseudo)
+    {
+        $stmt = $pdo -> prepare(('SELECT tel FROM tb_user WHERE pseudo = ?'));
+        $stmt -> bindParam(1, $pseudo);
+        $stmt -> execute();
+        $stmt = $stmt->fetch();
+        return $stmt[0];
+    }
+    function affichageAdresse($pdo,$pseudo)
+    {
+        $stmt = $pdo -> prepare(('SELECT adresse FROM tb_user WHERE pseudo = ?'));
+        $stmt -> bindParam(1, $pseudo);
+        $stmt -> execute();
+        $stmt = $stmt->fetch();
+        return $stmt[0];
+    }
+    function affichageMail($pdo,$pseudo)
+    {
+        $stmt = $pdo -> prepare(('SELECT email FROM tb_user WHERE pseudo = ?'));
+        $stmt -> bindParam(1, $pseudo);
+        $stmt -> execute();
+        $stmt = $stmt->fetch();
+        return $stmt[0];
+    }
+    function affichageProduct($pdo,$label,$string)
+    {
+        $stmt = $pdo -> prepare(('SELECT * FROM tb_product WHERE label_produit = ?'));
+        $stmt -> bindParam(1,$label);
+        $stmt -> execute();
+        $stmt = $stmt->fetch();
+        return $stmt[$string];
     }
 ?>
