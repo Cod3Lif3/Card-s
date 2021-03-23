dac01b7dacfa984151f1f31010fc7ee1f8e9b5f7
rouan <rouan.laporal@outlook.com>
2021-03-23 21:30:39 +0100

    no change

--- config/init.php
+++ config/init.php
@@ -1,7 +1,14 @@
 <?php
 
 // connexion PDO
-
+try
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=bd_card;','root','aZERTYUIOP_973',[PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
+}
+catch(Exception $e)
+{
+    die('Erreur : '. $e->getMessage());
+}
 //variable d'affichage
 
 //
